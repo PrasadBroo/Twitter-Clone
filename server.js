a77191de94b1e7247d78614042de4009e1553892
PrasadBroo <sketchware1@gmail.com>
2021-12-14 19:05:26 +0530

    hmm

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const helmet = require("helmet");
+const cors = require('cors');
+
+
+const app = express()
+const port =   process.env.PORT || 5000
+
+app.use(cors());
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
