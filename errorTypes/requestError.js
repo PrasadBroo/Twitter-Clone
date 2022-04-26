bdca2bdfa788e2b3cf8c4fa49d10475640cd2aad
PrasadBroo <sketchware1@gmail.com>
2022-04-26 13:31:14 +0530

    make it better

--- errorTypes/requestError.js
+++
@@ -1,7 +0,0 @@
-module.exports = class RequestError extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.name = 'RequestError';
-      this.statusCode = statusCode;
-    }
-  };
